49f12c133ce98d393f1f3fa359a497c70dc2f0c2
V Devanarayan <89205573+CodeTitan7@users.noreply.github.com>
2026-07-16 14:56:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.getElementById('clickBtn').addEventListener('click', () => {
   document.getElementById('output').textContent =
-    'You clicked the button at ' + new Date().toLocaleTimeString();
+    'Button clicked at ' + new Date().toLocaleTimeString();
 });
